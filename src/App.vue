180cc24fade08b93d75c70326a817403984dd9aa
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-01-24 11:50:02 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hi</h1>
+  <div v-if="showModal">
+    <Popup_Modal :theme="theme" @close="showModal=false">
+        <h1>Login Success</h1>
+        <p>Welcome user hlaing min than</p>
+        <template v-slot:links>
+            <a href="">Sign Up</a>
+            <a href="">Register</a>
+        </template>
+    </Popup_Modal>
+
+  </div>
+  <div v-if="deleteModal">
+    <Popup_Modal :themeDelete="themeDelete" @close="deleteModal=false">
+      <h1>Delete</h1>
+      <p>Are you sure want to delete?</p>
+    </Popup_Modal>
+  </div>
+  
+  <button @click="showModal = true">open modal</button>
+  <button @click="deleteModal = true">Delete</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Popup_Modal from './components/Popup_Modal.vue'
 
 export default {
-  name: 'App',
+  data(){
+    return{
+      theme : "success",
+      showModal : false,
+      themeDelete : "delete",
+      deleteModal : false
+    }
+  },
   components: {
-    HelloWorld
+    Popup_Modal
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1{
+    color: blue;
+  }
+  p{
+    font-style: normal;
+  }
 </style>
---
+++ src/components/Popup_Modal.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{success:theme=='success', danger:theme=='danger', delete:themeDelete=='delete'}">
+            <slot>
+                default
+            </slot>
+            <slot name="links"/>
+        </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+    props:['theme','themeDelete'],   // success
+    methods:{
+        closeModal(){
+            this.$emit("close")
+        }
+    }
+}
+</script>
+
+<style>
+    .backdrop{
+        position: fixed;
+        top: 0;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: white;
+        border-radius: 10px;
+    }
+    p{
+      font-style: normal;
+    }
+    .success{
+        background-color: green;
+        color: white;
+    }
+    .danger{
+        background-color: crimson;
+        color: white;
+    }
+    .delete{
+        background-color: crimson;
+        color: white;
+    }
+</style>
